dc7867ea7a64428f616bf8d84e25b5206a34742e
Vipr728 <vipr728@gmail.com>
2023-02-22 17:29:25 -0800

    TopKFrequeneElements

--- LeetCode/Arrays&Hashing/TopKFrequentElements.js
+++ LeetCode/Arrays&Hashing/TopKFrequentElements.js
@@ -18,4 +18,51 @@ Constraints:
 k is in the range [1, the number of unique elements in the array].
 It is guaranteed that the answer is unique.
 */
-TopKFrequentElements([1,1,1,2,2,3]);
+
+
+//we can use a hashmap to solve this in nlogn time here is how it would work:
+
+function TopKFrequentElementsHASHMAP(nums){
+    let map = new Map();
+    for(let i = 0; i< nums.length; i++){
+        if(map.has(nums[i]))
+            map.set(nums[i], map.get(nums[i])+1)
+        else 
+            map.set(nums[i], 1)
+    }
+    //then sort out the map and then return an array which is self-explanatory
+ }
+
+//another way is to use bucket sort and store the frequencies in the key instead of the value which will take O(n)
+function TopKFrequentElements(nums, k){
+    let map = {};
+    let bucket = [];
+    let result = [];
+    
+    for(let i = 0; i<nums.length; i++){
+        if(!map[nums[i]])
+            map[nums[i]] = 1 
+        else 
+            map[nums[i]]++
+    }
+    
+    for(let [num, freq] of Object.entries(map)){
+        if(!bucket[freq]){
+            bucket[freq] = new Set().add(num);
+        } else 
+            bucket[freq] = bucket[freq].add(num);
+    }
+
+    for(let i = bucket.length; i>=0; i--){
+        if(bucket[i])
+            result.push(...bucket[i])
+        if(result.length==k)
+            break;
+    }
+
+    console.log(map);
+    console.log(bucket);
+    return result;
+}
+
+console.log(TopKFrequentElements([1,1,1,2,2,3,3,3,3,3,3], 2))
